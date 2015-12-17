7678ed746a77099669496c1f6361b76dc8c72e10
Davidtsang <davidt2008cn@gmail.com>
2015-12-17 09:24:31 +0800

    add devise mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #send email use mailgun
+  config.action_mailer.default_url_options = { :host => "www.400px.net" }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :authentication => :plain,
