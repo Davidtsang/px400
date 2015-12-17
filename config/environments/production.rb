681c1739bb86a15a2f5c272c1b93aeac638f7af2
Davidtsang <davidt2008cn@gmail.com>
2015-12-17 09:30:14 +0800

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #send email use mailgun
-  config.action_mailer.default_url_options = { :host => "www.400px.net" }
+  config.action_mailer.default_url_options = { :host => "mg.400px.net" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
