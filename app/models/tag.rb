2ae30465733746bc9448ac247b0e7ba011881c91
Davidtsang <davidt2008cn@gmail.com>
2015-12-29 14:01:01 +0800

    hot fix tag null error

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ActiveRecord::Base
   has_many :users_tags
   has_many :works_tags
 
-  validates :name ,presence: true
+  validates :name ,presence: true, length: {minimum: 2, maximum: 40}
 
 
   def self.search(search, limit: 12)
--- app/models/users_tag.rb
+++ app/models/users_tag.rb
@@ -1,6 +1,7 @@
 class UsersTag < ActiveRecord::Base
   belongs_to :tag, counter_cache: :items_count
   belongs_to :user
+  validates_presence_of :user_id, :tag_id
 
   validates_uniqueness_of :user_id , scope: :tag_id
 
