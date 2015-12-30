b250d2610c884436b135785f2f9ae7db5ab9212f
Davidtsang <davidt2008cn@gmail.com>
2015-12-30 10:27:48 +0800

    hot fix user list and pm list error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  default_scope {order('created_at ASC')}
+  default_scope {order('created_at DESC')}
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
