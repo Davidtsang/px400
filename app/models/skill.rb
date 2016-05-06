2c40142eefdf95fcbaf8a31865552dc679630f01
Davidtsang <davidt2008cn@gmail.com>
2016-05-06 22:53:32 +0800

    fix style

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < Tag
 
   def self.all_by_users_domain_id(domain_id)
-    joins("JOIN users_tags ON users_tags.tag_id = tags.id").joins("JOIN users ON users.id = users_tags.user_id").where("users.domain_1_id = :domain_id OR users.domain_2_id = :domain_id", :domain_id=> domain_id).uniq
+    joins("JOIN users_tags ON users_tags.tag_id = tags.id").joins("JOIN users ON users.id = users_tags.user_id").where("users.domain_1_id = :domain_id OR users.domain_2_id = :domain_id", :domain_id=> domain_id).uniq.limit(100)
   end
 end
